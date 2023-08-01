b0ce6e63283d9cf130f0535532ad3636766bfd8d
StillRagdoll <138829720+StillRagdoll@users.noreply.github.com>
2023-08-01 18:35:00 -0400

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryBackend.Models;
+
+namespace MusicLibraryBackend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet <Song> Songs { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions
+            options) : base(options)
+            
+            {
+
+            }
+
+
+        
+    }
+}
